a2207d65bee4da8fc7003bbcc5b961a91396e61f
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2025-09-07 18:46:14 +0530

    Initialize React project with Vite, Tailwind CSS, and ESLint configuration. Added essential files including index.html, package.json, and various React components for routing and layout. Configured .gitignore to exclude build artifacts and logs.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      
+        <h1 className="bg-green-600 p-4 bg-center text-center">React router</h1>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Layout from "./Layout.jsx";
+import { RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import { Route } from "react-router-dom";
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+    <Route path = "" element={<Home />} />
+    <Route path = "about" element={<About />} />
+    <Route path = "contact" element={<Contact />} />
+  </Route>
+));
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
